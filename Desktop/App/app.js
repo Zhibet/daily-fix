37c074ccb2f0fb5cf6c8f6c951af45a436a2978f
Zhibet <baha819@cobleskill.edu>
2024-04-07 14:13:39 -0400

    finished the setup

--- Desktop/App/app.js
+++ Desktop/App/app.js
@@ -2,7 +2,12 @@ const express = require('express');
 const app = express();
 const ejs = require('ejs');
 const ejsMate = require('ejs-mate');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/bussiness');
+
 const homeRoute = require('./routes/home')
+
+app.use(express.static('public'))
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs'); 
 
